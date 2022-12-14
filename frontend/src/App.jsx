08a939de485f079e8c3a3dfc6ec9567d4f596b6f
Muhammad Ali Gulzar <ali@Muhammads-MBP-2.home>
2022-12-14 16:48:48 +0200

    addition: add env variables file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>Welcome to Brickly!</div>
+      <div>Welcome to Brickly! {'->'} {import.meta.env.VITE_BACKEND_URL}</div>
     </div>
   );
 }
